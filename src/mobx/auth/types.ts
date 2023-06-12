dfbc5a2c17f0930ad0456cb2e7aa559e3e72bc5b
Valeria Radchenko <leraradchenko818@gmail.com>
2023-06-13 02:05:54 +0300

    add logic

--- src/mobx/auth/types.ts
+++
@@ -1,4 +0,0 @@
-export interface ISignInUserBody {
-  telegramId: string;
-  password: string;
-}
--- src/validation/post.ts
+++
@@ -1,7 +0,0 @@
-import * as Yup from 'yup';
-
-export const DialogPostSchema = Yup.object().shape({
-  title: Yup.string().trim().required('Required'),
-  description: Yup.string().trim().required('Required'),
-  link: Yup.string().trim().required('Required'),
-});
